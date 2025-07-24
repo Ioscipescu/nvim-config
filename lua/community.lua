0bfcfd28f9c6053643850a065abebc730f7cb1b0
Ioscipescu <blountwy@gmail.com>
2025-07-24 10:17:15 -0600

    Added some more packages

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -9,4 +7,9 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.markdown-and-latex.vimtex" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.git.gitgraph-nvim" },
 }
